f615fd4bddfbc00f16af2dce24b4ab836b29bc30
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-17 15:22:12 +0900

    Material 디자인 RecycleView 와 CardView - 불필요한 코드 삭제

--- LionApp/app/src/main/java/com/oz10/lionapp/RecycleActivity.kt
+++ LionApp/app/src/main/java/com/oz10/lionapp/RecycleActivity.kt
@@ -22,19 +22,6 @@ class RecycleActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_recycle)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_recycle)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
